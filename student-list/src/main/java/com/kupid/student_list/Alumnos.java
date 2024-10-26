ad35074ebf83da23df90cd2d17858e39d78f1e18
“kupid” <shiftybroccoli74@gmail.com>
2024-10-26 20:47:12 -0300

    Added: Alumnos class

--- student-list/src/main/java/com/kupid/student_list/Alumnos.java
+++ student-list/src/main/java/com/kupid/student_list/Alumnos.java
@@ -1,5 +1,39 @@
 package com.kupid.student_list;
 
 public class Alumnos {
-
+	
+	private int id;
+	private String studentName;
+	private String studentLastName;
+	
+	public Alumnos(int id, String studentName, String studentLastName) {
+		this.id = id;
+		this.studentName = studentName;
+		this.studentLastName = studentLastName;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentLastName() {
+		return studentLastName;
+	}
+	public void setStudentLastName(String studentLastName) {
+		this.studentLastName = studentLastName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Alumnos [id=" + id + ", studentName=" + studentName + ", studentLastName=" + studentLastName + "]";
+	}
+	
 }
